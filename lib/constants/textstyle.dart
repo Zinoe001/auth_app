96fa1ebf8f91a3c0aa836d7e6508795590395690
Zinoe001 <zinoetech@gmail.com>
2022-02-17 08:01:53 +0100

    completed textstyles

---
+++ lib/constants/textstyle.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+TextStyle headingStyle1 = GoogleFonts.poppins(
+  fontSize: 24,
+  fontWeight: FontWeight.w600,
+);
+
+TextStyle headingStyle2 = GoogleFonts.poppins(
+  fontSize: 21,
+  fontWeight: FontWeight.w600,
+);
+
+TextStyle headingStyle3 = GoogleFonts.poppins(
+  fontSize: 18,
+  fontWeight: FontWeight.w600,
+);
+
+TextStyle headingStyle4 = GoogleFonts.poppins(
+  fontSize: 15,
+  fontWeight: FontWeight.w600,
+);
+
+TextStyle headingStyle5 = GoogleFonts.poppins(
+  fontSize: 12,
+  fontWeight: FontWeight.w600,
+);
+
+TextStyle headingStyle6 = GoogleFonts.poppins(
+  fontSize: 10,
+  fontWeight: FontWeight.w600,
+);
+
+TextStyle bodyStyle1 = GoogleFonts.dmSans(
+  fontSize: 10,
+  fontWeight: FontWeight.w500,
+);
+
+TextStyle bodyStyle2 = GoogleFonts.dmSans(
+  fontSize: 12,
+  fontWeight: FontWeight.w500,
+);
+TextStyle bodyStyle3 = GoogleFonts.dmSans(
+  fontSize: 14,
+  fontWeight: FontWeight.w500,
+);
+
+TextStyle captionText = GoogleFonts.yellowtail(
+  fontSize: 10,
+  fontWeight: FontWeight.w400,
+);
+TextStyle buttonText = GoogleFonts.montserrat(
+  fontSize: 12,
+  fontWeight: FontWeight.w400,
+);
